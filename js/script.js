f189df17aa6d690f2b014ffb7d30466e91c9ccac
mariaconejo <mary03081@gmail.com>
2020-07-29 17:18:38 -0600

    Correciones

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 // funcion que solo modifica el color del texto que contenga el div container
 let color = document.querySelectorAll('.container p')
 function colorRam() {   
-   let x=Math.floor(Math.random() * (256 - 0) + 0);
-   let y=Math.floor(Math.random() * (256 - 0) + 0);  //comienza de 0 a 256 para que incluya al 255
-   let z=Math.floor(Math.random() * (256 - 0) + 0);
+   let x=Math.floor(Math.random() * 256);
+   let y=Math.floor(Math.random() * 256);  //comienza de 0 a 256 para que incluya al 255
+   let z=Math.floor(Math.random() * 256);
 
     return `rgb(${x}, ${y}, ${z})`; // retorna un string con el rgb
 }
